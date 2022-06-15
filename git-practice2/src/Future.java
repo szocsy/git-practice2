a3d61096295e70034ba5cc58618cd4f712e0bef2
Ferenc Szécsy <szocsy@gmail.com>
2022-06-15 13:41:39 +0200

    e6

--- git-practice2/src/Future.java
+++ git-practice2/src/Future.java
@@ -1,7 +1,7 @@
 public class Future {
 
     public void future(String zodiac) {
-        System.out.println("Your horoscope sign is " + zodiac);
+        System.out.println("Your horoscope sign is " + zodiac + " the sign.");
     }
 
 }
