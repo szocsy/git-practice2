4e7a6de80c2d531d469ea915e52bc71d5455f490
Soma Bártfai <soma.bartfai@gmail.com>
2022-06-15 11:45:40 +0200

    c5

--- git-practice2/src/Cheer.java
+++ git-practice2/src/Cheer.java
@@ -1,6 +1,6 @@
 public class Cheer {
 
-    public void cheer(String txt) {
-        System.out.println("Riskies and the cats (and the failed)" + txt);
+    public void cheer(String txt, int num) {
+        System.out.println("Riskies and the cats (and the failed)" + txt + num);
     }
 }
