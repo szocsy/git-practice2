d2b04ee5cb9f64589fd8bb59e887060c4c3dd55c
Vass Tímea <t.varvari@gmail.com>
2022-06-15 12:15:18 +0200

    b5-v5 merge

--- git-practice2/src/Count.java
+++ git-practice2/src/Count.java
@@ -1,9 +1,6 @@
 public class Count {
     public void Count(String param1, String param2) {
-<<<<<<< HEAD
+
         System.out.println(param1 + " még izgibb... " + param2);
-=======
-        System.out.println(param1 + " " + param2);
->>>>>>> d6342a0917f4604245cd6857ff7bdfcb7d7e1e8e
     }
 }
