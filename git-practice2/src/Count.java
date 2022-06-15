3d1842aa9ae8cd8d70029b49c067a5ab022466c3
Soma Bártfai <soma.bartfai@gmail.com>
2022-06-15 13:41:48 +0200

    c6

--- git-practice2/src/Count.java
+++ git-practice2/src/Count.java
@@ -1,6 +1,6 @@
 public class Count {
     public void Count(String param1, String param2) {
 
-        System.out.println(param1 + " még izgibb... " + param2);
+        System.out.println(param1 + "Counting the countless" + " még izgibb... " + param2);
     }
 }
