d767c6ec278b3970cc671e61612b0d6f2a7d35ad
Vass Tímea <t.varvari@gmail.com>
2022-06-15 12:08:29 +0200

    b5-v3

--- git-practice2/src/Count.java
+++ git-practice2/src/Count.java
@@ -1,5 +1,5 @@
 public class Count {
     public void Count(String param1, String param2) {
-        System.out.println(param1 + " de izgi :D " + param2);
+        System.out.println(param1 + " még izgibb... " + param2);
     }
 }
